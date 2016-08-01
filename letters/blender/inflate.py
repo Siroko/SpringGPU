d87b415fefe3eb011ca678c03964ef1400f4ebe3
Valentin Marmonier <valentin.marmonier@b-reel.com>
2016-08-01 11:16:03 +0200

    update balloon blender script

--- letters/blender/inflate.py
+++ letters/blender/inflate.py
@@ -1,3 +1,8 @@
+EXTRUSION = -0.1
+FATTEN = -1.5
+SMOOTHNESS = 3
+HIDE = True
+
 import bpy
 
 # get selected objects
@@ -12,21 +17,34 @@ for obj in selected:
     bpy.ops.mesh.select_mode(type='FACE')
     bpy.ops.mesh.select_all(action='SELECT')
     
-    # extrude depth
-    bpy.ops.mesh.extrude_region_move(TRANSFORM_OT_translate={"value":(0, -0.1, 0)})
+    # extrude
+    bpy.ops.mesh.extrude_region_move(TRANSFORM_OT_translate={"value":(0, EXTRUSION, 0)})
     
     # select all new faces
     bpy.ops.mesh.select_all(action='SELECT')
     
     # fatten
-    bpy.ops.transform.shrink_fatten(value=-1.5, use_even_offset=False, mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
+    bpy.ops.transform.shrink_fatten(value=FATTEN, use_even_offset=False, mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
 
-    # back to normal
+    # back to object mode
     bpy.ops.object.mode_set(mode='OBJECT')
 
-    # smooth
-    bpy.ops.object.modifier_add(type='SUBSURF')
+    # center origin
+    bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
 
-    # then apply smooth
-    # create folds with sculpt tool
-    
+    # add smoothness
+    bpy.ops.object.modifier_add(type='SUBSURF')
+    bpy.context.object.modifiers["Subsurf"].levels = SMOOTHNESS
+
+    # apply smoothness
+    bpy.ops.object.modifier_apply(modifier='Subsurf')
+
+    # center
+    bpy.context.object.location = (0, 0, 0)
+
+    # apply location, scale and rotation
+    bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+
+    # hide
+    if HIDE:
+        bpy.context.object.hide = true
