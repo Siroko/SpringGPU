83e91555d5d8b5eaf9165118110d87087528b58c
Valentin Marmonier <valentin.marmonier@b-reel.com>
2016-09-05 17:39:28 +0200

    add function to get text and colors from hash

--- app/view/utils.js
+++ app/view/utils.js
@@ -11,8 +11,39 @@ function random(min, max, round) {
   return round ? Math.round(value) : value;
 };
 
+/**
+ * @function getTextAndColorsFromHash
+ * @returns {{[name:string]:string}}
+ */
+function getTextAndColorsFromHash() {
+  var hash = window.location.hash;
+
+  var text = null;
+  var colors = null;
+
+  hash.substr(1).split('&').forEach(function(part) {
+    var subpart = part.split('=');
+
+    if(subpart.length) {
+      if(subpart[0] === 'text') {
+        text = subpart[1].toUpperCase();
+      } else if(subpart[0] === 'colors') {
+        colors = subpart[1].toUpperCase();
+      }
+    }
+  });
+
+  return {
+    text: text || 'HELLO WORLD',
+    colors: colors || text ?
+      Array.prototype.map.call(text, function() { return 'S'; }).join('')
+      : 'SSSSS GGGGG'
+  }
+};
+
 module.exports = {
   random: random,
   textureLoader: new THREE.TextureLoader(),
-  jsonLoader: new THREE.JSONLoader()
+  jsonLoader: new THREE.JSONLoader(),
+  getTextAndColorsFromHash: getTextAndColorsFromHash
 };
